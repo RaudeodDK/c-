b766571be2e93ddbae178b885846e8c3f6843b79
RaudeodDK <hugoeduardo167@gmail.com>
2026-09-01 16:56:22 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Program
+{
+static void Main()
+
+{   Console.Write("Informe sua nota final: ");
+    double nota_final = double.Parse(Console.ReadLine());
+
+    Console.Write("Informe sua porcentagem de frequencia: ");
+    double frequencia = double.Parse(Console.ReadLine());
+    
+    if (nota_final >= 9 || frequencia >= 95)
+    {
+        Console.WriteLine("Você é um aluno destaque");
+    }
+    else
+    {
+        Console.WriteLine("Não atingiu os requisitos.");
+    }
+
+}
+}
